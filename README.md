d5835c6b5f64488c3383add350e54726877c0849
AJ Collins <16407457+ajc133@users.noreply.github.com>
2023-10-24 23:45:51 -0700

    add word map

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ## Word list
 
-https://github.com/dolph/dictionary/blob/master/popular.txt
+https://github.com/dolph/dictionary/blob/master/popular.txt + manually 
+remove the words that are less than three letters and more than ten.
+
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func readWordList(path string) ([]string, error) {
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
-        words := []string{}
+	words := []string{}
 	for {
 		line, err := reader.ReadString('\n')
 		if err == io.EOF {
@@ -26,18 +26,30 @@ func readWordList(path string) ([]string, error) {
 			fmt.Printf("error reading file %s", err)
 			return nil, err
 		}
-		fmt.Print(line)
-                words = append(words, strings.Split(line, "\n")[0])
+		words = append(words, strings.Split(line, "\n")[0])
 	}
-        return words, nil
+	return words, nil
 }
+
+func threeLetterMap(words []string) map[string][]string {
+	m := make(map[string][]string)
+	for _, word := range words {
+		substring := word[:3]
+		if m[substring] == nil {
+			m[substring] = []string{word}
+		} else {
+			m[substring] = append(m[substring], word)
+		}
+	}
+	return m
+}
+
 func main() {
 
-        words, _ := readWordList("wordlist.txt")
-        for _, word := range words{
-                fmt.Println(word)
-        }
-
-
+	words, _ := readWordList("wordlist.txt")
+	wordMap := threeLetterMap(words)
+	for trigraph, words := range wordMap {
+		fmt.Println(len(words), trigraph)
+	}
 
 }
